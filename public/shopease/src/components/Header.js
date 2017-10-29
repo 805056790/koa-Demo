370fec1ef105f47fe420431d558da0eb1b097d70
yzdd <yjs10086>
2017-10-29 20:24:30 +0800

    新开了接口用于实现在线的聊天系统

---
+++ public/shopease/src/components/Header.js
@@ -0,0 +1,20 @@
+/**
+ * Created by yzdd on 2017/10/29.
+ */
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({});
